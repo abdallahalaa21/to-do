bb072cfcfb515d53cbe9466e24b1b58895dbd6b9
Abdullah Alaa <abdallah.bodi@gmail.com>
2021-07-25 17:43:47 +0200

    add todo details

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Checkbox,
   ListItem,
   ListItemIcon,
-  ListItemText
+  ListItemText,
+  makeStyles
 } from '@material-ui/core';
 
-const ToDoComponent = ({ todo, updateComplete }) => (
-  <ListItem key={todo.id} dense>
-    <ListItemIcon>
-      <Checkbox
-        edge="start"
-        checked={todo?.completed}
-        disableRipple
-        inputProps={{ 'aria-labelledby': todo.id }}
-        onChange={async () => {
-          await updateComplete({
-            id: todo.id,
-            completed: !todo.completed
-          });
-        }}
+import Modal from 'components/Modal';
+
+const useStyles = makeStyles({
+  pointer: {
+    cursor: 'pointer'
+  }
+});
+
+const ToDoComponent = ({ todo, updateComplete }) => {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+
+  const handleOpen = useCallback(() => {
+    setOpen(true);
+  }, []);
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  return (
+    <ListItem key={todo.id} dense>
+      <ListItemIcon>
+        <Checkbox
+          edge="start"
+          checked={todo?.completed}
+          disableRipple
+          inputProps={{ 'aria-labelledby': todo.id }}
+          onChange={async () => {
+            await updateComplete({
+              id: todo.id,
+              completed: !todo.completed
+            });
+          }}
+        />
+      </ListItemIcon>
+      <ListItemText
+        id={todo.id}
+        primary={todo?.title}
+        secondary={todo?.description}
+        onClick={handleOpen}
+        className={classes.pointer}
       />
-    </ListItemIcon>
-    <ListItemText
-      id={todo.id}
-      primary={todo?.title}
-      secondary={todo?.description}
-    />
-  </ListItem>
-);
+      <Modal open={open} handleClose={handleClose}>
+        <p>title: {todo.title}</p>
+        <p>description: {todo.description}</p>
+        <p>due date: {todo.dueDate}</p>
+      </Modal>
+    </ListItem>
+  );
+};
 
 ToDoComponent.propTypes = {
   todo: PropTypes.object.isRequired,
--- src/pages/HomePage/hooks/gql/todos.gql.js
+++ src/pages/HomePage/hooks/gql/todos.gql.js
@@ -7,6 +7,7 @@ export default gql`
       title
       description
       completed
+      dueDate
     }
   }
 `;
